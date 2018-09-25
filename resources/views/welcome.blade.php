0338ac7b8458eb99be7070a3521d4404dcf7d598
rayokaah <raypex2@gmail.com>
2018-09-25 11:56:53 +0100

    Add image resources

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,12 +83,10 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://github.com/DSCLEADSAfrica/bole/projectreadme.md">Start Project Here</a>
+                    <a href="https://github.com/DSCLEADSAfrica/bole/blob/master/README.md">Bole ReadMe</a>
+                    <a href="https://github.com/DSCLEADSAfrica/bole">Bole on GitHub</a>
+                    <a href="https://github.com/DSCLEADSAfrica/">DSCLEADSAfrica</a>
                 </div>
             </div>
         </div>
